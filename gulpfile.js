c89ed498b7ee47fa77435e426b41f9d01b6b09a0
richardt <r.tugwell@forthmedia.com>
2016-02-27 11:36:45 +0000

    streamline task imports

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var gulp        = require('gulp');
 var sequence    = require('run-sequence');
+var utils = require('./gulp/utils');
 
 //config contains all constants and variables - passed to all tasks. Also parses command parms
 var config = require('./gulp/config');
 
-//recursively requires all task modules
-require('./gulp/utils').files('./gulp/tasks' , './tasks' , config);
+//recursively imports all task modules
+utils.readDir(config.TASKDIR)
+    .map(function(file) {
+        require(config.TASKDIR + file)(config);
+    });
 
 // (re)Build the target folder by running the defined tasks
 gulp.task('build', ['sass', 'javascript', 'images', 'copy', 'jekyll'] );
